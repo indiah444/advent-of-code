7d2b07eff54b1316b54e1999873f6eb883c9d8c4
India <india.howell123@gmail.com>
2024-09-09 16:01:15 +0100

    Committing solution for part 2 of puzzle.

--- 2015/day_1.py
+++ 2015/day_1.py
@@ -5,7 +5,8 @@ def read_input(filename: str) -> list[str]:
     """Opens and reads a file."""
 
     with open(filename, "r", encoding="UTF-8") as f:
-        return f.readlines()
+        data = f.readlines()
+        return data[0]
 
 
 def find_floor_no(direction: str) -> int:
@@ -24,5 +25,26 @@ def find_floor_no(direction: str) -> int:
     return floor_no
 
 
+def find_basement_position(direction: str) -> int:
+
+    floor_no = 0
+    index = 0
+
+    for char in direction:
+        index += 1
+        if char == "(":
+            floor_no += 1
+        if char == ")":
+            floor_no -= 1
+        if floor_no == -1:
+            return index
+
+
 if __name__ == "__main__":
-    print(find_floor_no(")())())"))
+    instructions = read_input("day_1_input.txt")
+    correct_floor = find_floor_no(instructions)
+    basement_position = find_basement_position(instructions)
+
+    print(f"The floor Santa needs to go to is {correct_floor}")
+    print(f"The position of the character that causes Santa to first enter the basement is {
+          basement_position}")
