bb6f23056e2adfd7845c94868f1e20aa7920bec8
India <india.howell123@gmail.com>
2024-09-16 14:11:09 +0100

    Solution for part 2 of day 1 2017 puzzle.

--- 2017/day_1.py
+++ 2017/day_1.py
@@ -17,7 +17,21 @@ def inverse_captcha(digits: int):
     return total
 
 
+def circular_list_captcha(digits: int):
+    digits = [int(d) for d in digits]
+    n = len(digits)
+    halfway = n // 2
+    total = 0
+
+    for i in range(n):
+        if digits[i] == digits[(i + halfway) % n]:
+            total += digits[i]
+
+    return total
+
+
 if __name__ == "__main__":
 
     input_lines = read_input("day_1_input.txt")
     print(inverse_captcha(input_lines))
+    print(circular_list_captcha(input_lines))
